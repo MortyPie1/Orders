da187ab2800ed15a0fceeedf5c2867709f18edfd
Mortypie1 <murtida33@gmail.com>
2025-05-10 14:25:53 +0300

    first

---
+++ database/migrations/2025_05_08_115149_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_type')->default('normal'); //normal , urgent
+            $table->string('order_status')->default('pending'); //pending , out_for_delivery , delivered
+            $table->foreignId('driver_id')->constrained('drivers');
+            $table->foreignId('admin_id')->constrained('admins');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
